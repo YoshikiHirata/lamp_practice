74c078d1823035fd8d762b0a01c7efa601841d96
YoshikiHirata <yoshiki.hrt@gmail.com>
2021-01-13 08:16:02 +0900

    SQLインジェクション対策修正-3

--- www/model/item.php
+++ www/model/item.php
@@ -82,7 +82,7 @@ function insert_item($db, $name, $price, $stock, $filename, $status){
         image,
         status
       )
-    VALUES('?', ?, ?, '?', ?);
+    VALUES(?, ?, ?, ?, ?);
   ";
 
   return execute_query($db, $sql, [$name, $price, $stock, $filename, $status_value]);
@@ -99,7 +99,7 @@ function update_item_status($db, $item_id, $status){
     LIMIT 1
   ";
   
-  return execute_query($db, $sql, $item_id);
+  return execute_query($db, $sql, [$item_id]);
 }
 
 function update_item_stock($db, $item_id, $stock){
